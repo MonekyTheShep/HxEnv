bd8c6a50c7ccd92265a1813897ac179d38008c03
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-03-12 18:28:21 +0000

    refactor (utils) normalise the value if it has any characters required in quotes

---
+++ src/hxenv/Utils.hx
@@ -0,0 +1,26 @@
+package hxenv;
+
+class Util {
+    public static function normaliseValue(value:String) {
+        if (value.length == 0) return "";
+
+        var needQuotes:Bool = false;
+
+        for (i in 0...value.length) {
+            final char:Int = value.charCodeAt(i);
+
+            if (char == '\n'.code || char == ' '.code || char == "'".code || char == '"'.code) {
+                needQuotes = true;
+                break;
+            }
+        }
+
+        if (needQuotes) {
+            final escaped:String = StringTools.replace(value, '"', '\"');
+            return '"${escaped}"';
+        }
+
+        return value;
+
+    }
+}
